b770a8d8f4ddbbc257f1b289ce30bf43a7e1dca9
Evan <evulliam@sfu.ca>
2021-04-02 11:40:44 -0700

    first push

---
+++ database.py
@@ -0,0 +1,234 @@
+import mysql.connector
+
+
+class Database():
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def createConnection(self):
+        config = {
+            'user': 'root',  # default user for MySQL
+            'password': 'Root_default1996',  # whatever password you set
+            'host': 'localhost',
+            'database': 'ShoppingApplication',
+            'port': '3306',
+            'raise_on_warnings': True,
+
+        }
+        conn = mysql.connector.connect(**config)
+        return conn
+
+    def insertUser(self, userID, Username, Password, Email, StreetName, UnitNumber):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`User` " \
+                f"(`userID`, `Username`, `Password`, `Email`, `StreetName`, `UnitNumber`) " \
+                f"VALUES ('{userID}', '{Username}', '{Password}', '{Email}', '{StreetName}', '{UnitNumber}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertUserPhone(self, userID, PhoneNumber):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`UserPhoneNumber` " \
+                f"(`userID`, `PhoneNumber`) " \
+                f"VALUES ('{userID}', '{PhoneNumber}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertCustomer(self, userID, FirstName, LastName, CustomerRating):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`Customer` " \
+                f"(`userID`, `FirstName`, `LastName`, `CustomerRating`) " \
+                f"VALUES ('{userID}', '{FirstName}', '{LastName}', '{CustomerRating}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertGrocery(self, userID, StoreName):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`GroceryStore` " \
+                f"(`userID`, `StoreName`) " \
+                f"VALUES ('{userID}', '{StoreName}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertSale(self, SaleItem, GroceryID, Start, End, Discount):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`SalePromotion` " \
+                f"(`SaleItem`, `GroceryId`, `SaleStart`, `SaleEnd`, `Discount`) " \
+                f"VALUES ('{SaleItem}', '{GroceryID}', '{Start}', '{End}', '{Discount}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertRating(self, Rater, Ratee, Upvote, DownVote):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`Rates` " \
+                f"(`raterID`, `rateeID`, `Upvote`, `Downvote`) " \
+                f"VALUES ('{Rater}', '{Ratee}', '{Upvote}', '{DownVote}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertCustomerList(self, ListNumber, CreationDate, CustomerID):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`CustomerList` " \
+                f"(`ListNumber`, `CreationDate`, `CustomerID`) " \
+                f"VALUES ('{ListNumber}', '{CreationDate}', '{CustomerID}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertListItem(self, ItemName, ListNumber, CustomerID, Quantity):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`ListItem` " \
+                f"(`ItemName`, `ListNumber`, `CustomerID`, `Quantity`) " \
+                f"VALUES ('{ItemName}', '{ListNumber}', '{CustomerID}', '{Quantity}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertParty(self, groupID, groupName, creatorID, numberOfMembers, shoppingDate):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`Party` " \
+                f"(`groupID`, `groupName`, `creatorID`,  `numberOfMembers`, `shoppingDate`) " \
+                f"VALUES ('{groupID}', '{groupName}', '{creatorID}', '{numberOfMembers}', '{shoppingDate}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertGroupMembers(self, groupID, memberID):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`GroupMembers` " \
+                f"(`groupID`, `memberID`) " \
+                f"VALUES ('{groupID}', '{memberID}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertCreatesGroup(self, customerID, groupID):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`CreatesGroup` " \
+                f"(`customerID`, `groupID`) " \
+                f"VALUES ('{customerID}', '{groupID}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertViewsGroup(self, customerID, groupID):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`ViewsGroup` " \
+                f"(`customerID`, `groupID`) " \
+                f"VALUES ('{customerID}', '{groupID}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertJoinsGroup(self, customerID, groupID):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`JoinsGroup` " \
+                f"(`customerID`, `groupID`) " \
+                f"VALUES ('{customerID}', '{groupID}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertSaleShownToGroup(self, groupID, saleItem, groceryID):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`SaleShownToGroup` " \
+                f"(groupID, saleItem, groceryID) " \
+                f"VALUES ('{groupID}', '{saleItem}', '{groceryID}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    def insertReadCustList(self, ListNumber, groupID):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`ReadCustList` " \
+                f"(ListNumber, groupID) " \
+                f"VALUES ('{ListNumber}', '{groupID}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+
+    # This select can probably change. I was just messing around with joining the subclass to the superclass.
+    def selectCustomer(self, userID):
+        conn = self.createConnection(self)
+        c = conn.cursor(buffered=True)
+        query = f"SELECT * FROM Customer INNER JOIN User " \
+                f"ON Customer.userID = User.userID WHERE Customer.userID = '{userID}'"
+        c.execute(query)
+        conn.commit()
+        results = c.fetchall()
+        print(results)
+        c.close()
+        conn.close()
+
+    def selectUser(self, userID):
+        conn = self.createConnection(self)
+        c = conn.cursor(buffered=True)
+        query = f"SELECT * FROM User WHERE userID = '{userID}'"
+        c.execute(query)
+        conn.commit()
+        results = c.fetchall()
+        print(results)
+        c.close()
+        conn.close()
+
+    def selectAllCustList(self):
+        conn = self.createConnection(self)
+        c = conn.cursor(buffered=True)
+        query = f"SELECT * FROM CustomerList"
+        c.execute(query)
+        conn.commit()
+        results = c.fetchall()
+        print(results)
+        c.close()
+        conn.close()
+
+    def selectAllItems(self, custID, listNumber):
+        conn = self.createConnection(self)
+        c = conn.cursor(buffered=True)
+        query = f"SELECT L.ItemName, L.Quantity " \
+                f"FROM ListItem L " \
+                f"WHERE customerID = '{custID}' AND listNumber = '{listNumber}'"
+        c.execute(query)
+        conn.commit()
+        results = c.fetchall()
+        print(type(results))
+        c.close()
+        conn.close()
+        return results
---
+++ main.py
@@ -0,0 +1,96 @@
+from flask import Flask, request
+from flask import render_template
+
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Root_default1996'
+app.config['MYSQL_DB'] = 'ShoppingApplication'
+
+mysql = MySQL(app)
+
+custID = '100000001'
+listNumber = '1'
+
+'''
+    def insertCustomerList(self, ListNumber, Name, CreationDate, CustomerID):
+        conn = self.createConnection(self)
+        c = conn.cursor()
+        query = f"INSERT INTO `ShoppingApplication`.`CustomerList` " \
+                f"(`ListNumber`, `Name`, `CreationDate`, `CustomerID`) " \
+                f"VALUES ('{ListNumber}', '{Name}','{CreationDate}', '{CustomerID}')"
+        c.execute(query)
+        conn.commit()
+        c.close()
+        conn.close()
+        
+        
+            elif request.method == "GET" and listNumber is not None:
+        cur = mysql.connection.cursor()
+        query = f"SELECT L.ItemName, L.Quantity " \
+                f"FROM ListItem L " \
+                f"WHERE customerID = '{custID}' AND listNumber = '{listNumber}'"
+        cur.execute(query)
+        mysql.connection.commit()
+        results = cur.fetchall()
+        cur.close()
+        return render_template('viewlists.html', name=results)
+
+'''
+
+'''
+       cur = mysql.connection.cursor()
+       query2 = f"SELECT Name " \
+               f"FROM CustomerList " \
+               f"WHERE customerID = '{custID}' AND listNumber = '{listNumber}'"
+       cur.execute(query2)
+       mysql.connection.commit()
+       results2 = cur.fetchall()
+       cur.close()
+       '''
+
+
+'''
+    query = f"SELECT Name, CreationDate FROM CustomerList WHERE CustomerID = '{custID}'"
+'''
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/list/", methods=['GET', 'POST'])
+def list():
+    if request.method == "GET":
+        cur = mysql.connection.cursor()
+        query = f"SELECT Name, CreationDate, listNumber FROM CustomerList WHERE CustomerID = '{custID}'"
+        cur.execute(query)
+        mysql.connection.commit()
+        results2 = cur.fetchall()
+        cur.close()
+        return render_template('viewlists.html', lists=results2)
+    elif request.method == "POST":
+        data = request.form.get('listButton')
+        print(data)
+    return render_template('viewLists.html')
+
+
+@app.route("/items/", methods=['GET', 'POST'])
+def items():
+    if request.method == "GET":
+        cur = mysql.connection.cursor()
+        query = f"SELECT L.ItemName, L.Quantity " \
+                f"FROM ListItem L " \
+                f"WHERE customerID = '{custID}' AND listNumber = '{listNumber}'"
+        cur.execute(query)
+        mysql.connection.commit()
+        results = cur.fetchall()
+        cur.close()
+        return render_template('listitems.html', list=results)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
